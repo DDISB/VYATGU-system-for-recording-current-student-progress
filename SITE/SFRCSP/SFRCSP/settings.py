8a6e2fa0233137027d5a44affcca9bc3f15a2871
Demid <disbden@gmail.com>
2024-02-05 21:44:14 +0300

    Подключена БД (settings.py - проекта), создано приложение ElectronicJournal

--- SITE/SFRCSP/SFRCSP/settings.py
+++ SITE/SFRCSP/SFRCSP/settings.py
@@ -74,9 +74,16 @@ WSGI_APPLICATION = 'SFRCSP.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': { 
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'electronicjournal', 
+        'USER': 'root', 
+        'PASSWORD': 'mysql', 
+        'HOST': 'localhost',
+        'PORT': '3306', 
+        # 'OPTIONS': { 
+        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" 
+        # } 
     }
 }
 
